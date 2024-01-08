758a1075b3b9179d3a15f46f2d36da9395e78e7f
rKaushalya <ravinduk56@gmail.com>
2024-01-08 14:37:21 +0530

    Router Part Done and Navigation Add

---
+++ client/src/view/common/DefaultLayout/DefaultLayout.tsx
@@ -0,0 +1,9 @@
+import {Component} from "react";
+
+export class DefaultLayout extends Component {
+    render() {
+        return (
+            <></>
+        );
+    }
+}
---
+++ client/src/view/common/NavBar/NavBar.tsx
@@ -0,0 +1,9 @@
+import {Component} from "react";
+
+export class NavBar extends Component {
+    render() {
+        return (
+            <></>
+        );
+    }
+}
