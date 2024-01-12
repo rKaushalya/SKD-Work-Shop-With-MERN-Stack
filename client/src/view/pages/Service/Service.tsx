13be3c883b0cfd76864c91786a3e6466993b8cf4
rKaushalya <ravinduk56@gmail.com>
2024-01-12 22:50:44 +0530

    Route Bug fixed

--- client/src/view/pages/Service/Service.tsx
+++ client/src/view/pages/Service/Service.tsx
@@ -1,101 +1,43 @@
 import {Component} from "react";
-import service from "../../../image/service2.jpg";
 import {Link} from "react-router-dom";
+import image1 from "../../../image/image1.jpg";
+import image2 from "../../../image/image2.jpg";
 
 export class Service extends Component {
     render() {
         return (
-            <div className="pt-20 ">
-                <div className="relative text-center">
-                    <img className="blur-sm h-[40em] w-full" src={service} alt="Image"/>
-                    <h1 className="absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2
-                                    text-white p-2 text-4xl font-bold rounded-2xl">
-                        Looking for The right<br/> Vehicle repair service?</h1>
-                    <p className="absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2
-                                    text-white pt-40">
-                        We are here to help you for any kind of error in your vehicle.<br/>
-                        we are providing valuable service for you.!</p>
-                    <div className="flex absolute top-2/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                        <button
-                            className="bg-red-800 p-3 m-1 font-bold text-white font-serif text-[14px] rounded-lg hover:scale-110 hover:shadow-lg">
-                            <Link to="/addbooking">Add Booking</Link></button>
-                        <button
-                            className="bg-blue-800 p-3 m-1 font-bold text-white font-serif text-[14px] rounded-lg hover:scale-110 hover:shadow-lg">
-                            <Link to="/service">Our Services</Link></button>
+            <div className="pt-20">
+                <div className="flex justify-evenly">
+                    <div>
+                        <h1 className="text-4xl font-bold text-red-600 mt-5 border-b-2 pb-2 mb-2">Book Car Repairs</h1>
+                        <p className="font-serif mb-3">Discover the ideal haven for your creative pursuits at our
+                            workshop-centric garage
+                            space.<br/>
+                            Our meticulously organized garage provides the perfect setting for hands-on projects,<br/>
+                            whether you're a hobbyist, DIY enthusiast, or a professional seeking a dedicated
+                            workspace.<br/>
+                            Equipped with essential tools, ample room for your projects, and a welcoming
+                            environment,<br/>
+                            our garage invites you to explore your passions and bring your ideas to life.<br/>
+                            Join us in a space designed to fuel innovation and craftsmanship."
+                        </p>
+
+                        <button className="bg-gradient-to-l from-red-400 to-blue-200 p-2 rounded-2xl m-5 font-serif
+                                           hover:scale-110 hover:shadow-lg">
+                            Click for more
+                        </button>
+                        <button className="bg-gradient-to-l from-red-400 to-blue-200 p-2 rounded-2xl m-5 font-serif
+                                           hover:scale-110 hover:shadow-lg">
+                            <Link to="/about">About Us</Link></button>
+                    </div>
+
+                    <div>
+                        <img className="h-72 mt-5 rounded-2xl" src={image1} alt="This is an image"/>
                     </div>
                 </div>
 
-                <div className="text-center absolute flex justify-center w-full mt-[-60px]">
-                    <div className="h-64 w-72 bg-blue-800">
-                        <h1 className="font-bold text-amber-50 mt-5 text-[18px]">Contact Details</h1>
-                    </div>
-                    <div className="h-64 w-72 bg-red-800">
-                        <h1 className="font-bold text-amber-50 mt-5 text-[18px]">Make an appointment</h1>
-
-                        <form className="w-10/12 ml-3">
-                            <div className="flex p-1 mt-5">
-
-                                <div className="">
-                                    <input
-                                        className="bg-red-900 appearance-none border-2 border-gray-700 rounded w-full
-                                            text-white focus:outline-blue-500 text-[12px] p-1 ml-2"
-                                        id="inline-full-name" type="text" placeholder="Your Full Name"/>
-                                </div>
-
-
-                                <div className="">
-                                    <input
-                                        className="bg-red-900 appearance-none border-2 border-gray-700 rounded w-full
-                                            text-white focus:outline-blue-500 text-[12px] p-1 ml-3"
-                                        id="inline-password" type="text" placeholder="Email"/>
-                                </div>
-
-                            </div>
-
-                            <div className="flex p-1">
-
-                                <div className="">
-                                    <input
-                                        className="bg-red-900 appearance-none border-2 border-gray-700 rounded w-full
-                                            text-white focus:outline-blue-500 text-[12px] p-1 ml-2"
-                                        id="inline-full-name" type="text" placeholder="Your Phone Number"/>
-                                </div>
-
-
-                                <div className="">
-                                    <input
-                                        className="bg-red-900 appearance-none border-2 border-gray-700 rounded w-full
-                                            text-white focus:outline-blue-500 text-[12px] p-1 ml-3"
-                                        id="inline-password" type="text" placeholder="City"/>
-                                </div>
-
-                            </div>
-
-
-                            <div className="mb-2">
-                                <input
-                                    className="bg-red-900 appearance-none border-2 border-gray-700 rounded w-full
-                                            text-white focus:outline-blue-500 text-[12px] ml-3 pb-6"
-                                    id="inline-full-name" type="text" placeholder="Message"/>
-                            </div>
-
-
-                            <div className="md:flex md:items-center">
-
-                                <button
-                                    className="shadow bg-white hover:bg-purple-400 font-bold py-2 px-4 rounded text-[14px]
-                                               w-full ml-3"
-                                    type="button">
-                                    Send Message
-                                </button>
-
-                            </div>
-                        </form>
-
-                    </div>
-                    <div className="h-64 w-72 bg-blue-800">
-                        <h1 className="font-bold text-amber-50 mt-5 text-[18px]">Maintenance Service</h1>
-                    </div>
+                <div className="max-w-full flex justify-center">
+                    <img className="w-10/12 rounded-2xl mb-3" src={image2} alt="This is an image"/>
                 </div>
 
             </div>
