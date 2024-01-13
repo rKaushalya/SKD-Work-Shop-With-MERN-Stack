d791aef16fa2826286c2ee84a3520508613a19f0
rKaushalya <ravinduk56@gmail.com>
2024-01-13 23:10:32 +0530

    Contact us page design finished

--- client/src/view/pages/Contact/Contact.tsx
+++ client/src/view/pages/Contact/Contact.tsx
@@ -5,62 +5,63 @@ export class Contact extends Component {
         return (
             <div className="h-lvh pt-20 flex justify-evenly">
 
-                <div className="h-vh bg-secondary w-1/3">
-                    <h1>Contact us</h1>
+                <div className="h-vh w-1/3">
+                    <h1 className="text-5xl font-serif ml-8 mt-52 text-gray-800">Contact us</h1>
+                    <p className="font-[13px] text-gray-800 ml-8 mt-10 font-serif">Need to get in touch with us? Either
+                        fill out the form with your inquiry or
+                        find the <a className="text-blue-800 underline" href="">document email </a>you'd like to
+                        contact below.</p>
                 </div>
 
-                <form className="ml-3 border-2 h-96">
-                    <div className="flex mt-5">
+                <form className="ml-3 border-2 h-96 mt-32 rounded shadow-lg shadow-gray-500">
+                    <div className="w-11/12 ml-7">
+                        <div className="flex mt-5 mb-3">
 
-                        <div className="">
-                            <label className="text-xs">First Name</label>
+                            <div className="">
+                                <label className="text-xs">First Name</label>
+                                <input
+                                    className="bg-gray-200 appearance-none rounded w-11/12
+                                            focus:outline-gray-400 text-[12px] p-2"
+                                    id="inline-full-name" type="text"/>
+                            </div>
+
+
+                            <div className="">
+                                <label className="text-xs">Last Name</label>
+                                <input
+                                    className="bg-gray-200 appearance-none rounded w-11/12
+                                            focus:outline-gray-400 text-[12px] p-2"
+                                    id="inline-password" type="text"/>
+                            </div>
+
+                        </div>
+
+                        <div className="mb-3">
+                            <label className="text-xs mr-5">Email</label>
                             <input
-                                className="bg-red-900 appearance-none border-2 border-gray-700 rounded w-full
-                                            text-white focus:outline-blue-500 text-[12px] p-1"
+                                className="bg-gray-200 appearance-none rounded w-11/12
+                                           focus:outline-gray-400 text-[12px] p-2"
+                                id="inline-full-name" type="email"/>
+                        </div>
+
+                        <div className="mb-5">
+                            <label className="text-xs">Message</label>
+                            <input
+                                className="bg-gray-200 appearance-none rounded w-11/12
+                                           focus:outline-gray-400 text-[12px] pb-12"
                                 id="inline-full-name" type="text"/>
                         </div>
 
+                        <div className="md:flex md:items-center">
 
-                        <div className="">
-                            <label className="text-xs">Last Name</label>
-                            <input
-                                className="bg-red-900 appearance-none border-2 border-gray-700 rounded w-full
-                                            text-white focus:outline-blue-500 text-[12px] p-1"
-                                id="inline-password" type="text"/>
+                            <button
+                                className="shadow bg-blue-500 text-white hover:bg-blue-900 font-bold py-2 px-4 rounded
+                                text-[14px]"
+                                type="button">
+                                Send Message
+                            </button>
+
                         </div>
-
-                    </div>
-
-
-                        <div className="">
-                            <label className="text-xs">Email</label>
-                            <input
-                                className="bg-red-900 appearance-none border-2 border-gray-700 rounded w-full
-                                            text-white focus:outline-blue-500 text-[12px] p-1"
-                                id="inline-full-name" type="text"/>
-                        </div>
-
-
-
-
-                    <div className="mb-2">
-                        <label className="text-xs">Message</label>
-                        <input
-                            className="bg-red-900 appearance-none border-2 border-gray-700 rounded w-full
-                                            text-white focus:outline-blue-500 text-[12px] pb-6"
-                            id="inline-full-name" type="text"/>
-                    </div>
-
-
-                    <div className="md:flex md:items-center">
-
-                        <button
-                            className="shadow bg-white hover:bg-purple-400 font-bold py-2 px-4 rounded text-[14px]
-                                               w-full"
-                            type="button">
-                            Send Message
-                        </button>
-
                     </div>
                 </form>
             </div>
