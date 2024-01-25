3236b9b9fabef7f868fa7d61e55494240a8a7921
rKaushalya <ravinduk56@gmail.com>
2024-01-25 19:57:28 +0530

    Data frtching part done

--- client/src/view/pages/ViewBooking/ViewBooking.tsx
+++ client/src/view/pages/ViewBooking/ViewBooking.tsx
@@ -1,7 +1,41 @@
 import {Component} from "react";
+import axios from "axios";
 
 export class ViewBooking extends Component {
+
+    private api: any;
+
+    constructor(props: {} | Readonly<{}>) {
+        super(props);
+        this.api = axios.create({baseURL: `http://localhost:4000`});
+        this.state = {
+            data: [],
+        }
+    }
+
+    componentDidMount() {
+        this.fetchData();
+    }
+
+    fetchData = async () => {
+        try {
+            this.api.get('/booking/all')
+                .then((res: { data: any }) => {
+                    const jsonData = res.data;
+                    this.setState({data: jsonData});
+                }).catch((error: any) => {
+                console.error('Axios Error:', error)
+            });
+        } catch (e) {
+            console.log("Error fetching Data.!");
+        }
+    }
+
     render() {
+
+        //@ts-ignore
+        const {data} = this.state;
+
         return (
             <div className="h-auto pt-20">
 
@@ -34,85 +68,39 @@ export class ViewBooking extends Component {
                         </tr>
                         </thead>
                         <tbody>
-                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                            <th scope="row"
-                                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                Apple MacBook Pro 17"
-                            </th>
-                            <td className="px-6 py-4">
-                                Silver
-                            </td>
-                            <td className="px-6 py-4">
-                                Laptop
-                            </td>
-                            <td className="px-6 py-4">
-                                $2999
-                            </td>
-                            <td className="px-6 py-4">
-                                $2999
-                            </td>
-                            <td className="px-6 py-4">
-                                $2999
-                            </td>
-                            <td className="px-6 py-4">
-                                $2999
-                            </td>
-                        </tr>
 
-                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                            <th scope="row"
-                                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                Apple MacBook Pro 17"
-                            </th>
-                            <td className="px-6 py-4">
-                                Silver
-                            </td>
-                            <td className="px-6 py-4">
-                                Laptop
-                            </td>
-                            <td className="px-6 py-4">
-                                $2999
-                            </td>
-                            <td className="px-6 py-4">
-                                $2999
-                            </td>
-                            <td className="px-6 py-4">
-                                $2999
-                            </td>
-                            <td className="px-6 py-4">
-                                $2999
-                            </td>
-                        </tr>
+                        {
+                            data.map((booking: any) => (
+                                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                    <th scope="row"
+                                        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        {booking.vehicleName}
+                                    </th>
+                                    <td className="px-6 py-4">
+                                        {booking.name}
+                                    </td>
+                                    <td className="px-6 py-4">
+                                        {booking.email}
+                                    </td>
+                                    <td className="px-6 py-4">
+                                        {booking.city}
+                                    </td>
+                                    <td className="px-6 py-4">
+                                        {booking.message}
+                                    </td>
+                                    <td className="px-6 py-4">
+                                        {booking.date}
+                                    </td>
+                                    <td className="px-6 py-4">
+                                        {booking.status}
+                                    </td>
+                                </tr>
+                            ))
+                        }
 
-                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                            <th scope="row"
-                                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                Apple MacBook Pro 17"
-                            </th>
-                            <td className="px-6 py-4">
-                                Silver
-                            </td>
-                            <td className="px-6 py-4">
-                                Laptop
-                            </td>
-                            <td className="px-6 py-4">
-                                $2999
-                            </td>
-                            <td className="px-6 py-4">
-                                $2999
-                            </td>
-                            <td className="px-6 py-4">
-                                $2999
-                            </td>
-                            <td className="px-6 py-4">
-                                $2999
-                            </td>
-                        </tr>
                         </tbody>
                     </table>
                 </div>
-
-                {/*<img className="w-full  filter opacity-80" src={image} alt="This is an image.!"/>*/}
 
             </div>
         );
--- server/routes/booking.js
+++ server/routes/booking.js
@@ -4,6 +4,6 @@ const express = require("express");
 var router = express.Router();
 
 router.post("/add",BookingController.addBooking);
-router.get("/all",BookingController.getBooking)
+router.get("/all",BookingController.getBooking);
 
 module.exports = router;
