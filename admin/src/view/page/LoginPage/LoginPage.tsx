e6fb529c9505b69ab0c094035d7dab8db66e4ccc
rKaushalya <ravinduk56@gmail.com>
2024-01-27 22:41:13 +0530

    Admin login part success

--- admin/src/view/page/LoginPage/LoginPage.tsx
+++ admin/src/view/page/LoginPage/LoginPage.tsx
@@ -1,14 +1,34 @@
 import {Component} from "react";
 import logo from "../../../image/logo.png";
-import {Link} from "react-router-dom";
+import axios from "axios";
 
-export class LoginPage extends Component {
+interface LoginProps {
+    data: any;
+}
+
+interface LoginState {
+    username: string;
+}
+
+export class LoginPage extends Component<LoginProps,LoginState> {
+
+    private api : any;
+
+    constructor(props: any) {
+        super(props);
+        this.api = axios.create({baseURL: `http://localhost:4000`});
+        this.state = {
+            username: ''
+        }
+        this.handleMessageInputOnChange = this.handleMessageInputOnChange.bind(this);
+    }
+
     render() {
         return (
             <div>
                 <div className="flex justify-center pt-10 pb-20">
                     <div
-                        className="bg-gray-100 w-[25em] h-[35em] mt-14 rounded-lg text-center shadow-lg shadow-gray-500">
+                        className="bg-gray-100 w-[25em] h-[35em] mt-14 rounded-lg text-center border-2">
                         <h1 className="text-4xl font-bold text-gray-200 bg-blue-400 pt-4 pb-4 rounded-t-lg">Login</h1>
 
                         <img className="h-36 ml-[8em]" src={logo} alt="this is an logo.!"/>
@@ -23,9 +43,9 @@ export class LoginPage extends Component {
                                             className="appearance-none rounded w-11/12 outline-gray-800
                                             focus:outline-blue-400 text-[16px] p-2 border-2 border-gray-300"
                                             id="inline-full-name" type="email"
-                                            name={"email"}
-                                            /*value={this.state.email}
-                                            onChange={this.handleMessageInputOnChange}*/
+                                            name={"username"}
+                                            value={this.state.username}
+                                            onChange={this.handleMessageInputOnChange}
                                         />
                                     </div>
 
@@ -42,8 +62,8 @@ export class LoginPage extends Component {
                                 <button
                                     className="shadow bg-blue-400 w-11/12 text-white hover:bg-blue-900 font-bold py-2 px-4 rounded
                                 text-[16px] mt-5"
-                                    type="button">
-                                    <Link to="/allbooking">Login</Link>
+                                    type="button" onClick={this.checkAdmin}>
+                                    Login
                                 </button>
                             </div>
                         </form>
@@ -51,5 +71,34 @@ export class LoginPage extends Component {
                 </div>
             </div>
         );
+    }
+
+    handleMessageInputOnChange(event: { target: { value: any; name: any; } }) {
+        const target = event.target;
+        const name = target.name;
+        const value = target.value;
+        // @ts-ignore
+        this.setState({
+            [name]: value
+        });
+    }
+
+    private checkAdmin = () => {
+
+        try {
+            this.api.get('/admin/check/'+this.state.username)
+                .then((res: { data: any }) => {
+                    const jsonData = res.data;
+                    if (jsonData[0].username === this.state.username){
+                        window.location.href = '/allbooking';
+                    }else {
+                        alert('Wrong Email or password.!');
+                    }
+                }).catch((error: any) => {
+                alert('Wrong Email or password.!');
+            });
+        }catch (e) {
+            console.log("Error fetching Data.!",e);
+        }
     }
 }
