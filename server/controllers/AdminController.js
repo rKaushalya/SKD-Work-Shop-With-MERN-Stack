cb600ed3b79c44c83bc3c3580bc99d6ec82d9c71
rKaushalya <ravinduk56@gmail.com>
2024-01-27 22:09:33 +0530

    Admin save backend created

--- server/controllers/AdminController.js
+++ server/controllers/AdminController.js
@@ -10,6 +10,15 @@ const AdminController = {
             console.error(e);
             res.status(500).json({error : 'Something went wrong.!'});
         }
+    },
+    checkAdmin: async function (req, res, next) {
+        try {
+            const username = req.params.username;
+            const admin = await Admin.find({username: username});
+            res.status(200).json(admin);
+        } catch (e) {
+            console.error('Something Went Wrong.!');
+        }
     }
 }
 
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -4,5 +4,6 @@ const express = require("express");
 var router = express.Router();
 
 router.post("/add",AdminController.saveAdmin);
+router.get("/check/:username",AdminController.checkAdmin);
 
 module.exports = router;
